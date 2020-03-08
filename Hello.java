dd28e55f7bcc00b9f53042ab02ddb7330a95ecc9
AhababOsama <ahababbinhabibosama2@gmail.com>
2020-03-09 02:49:34 +0600

    File Edited

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
   public static void main(String[] args) {
     System.outprintIn ("Visual Studio Code is Awesome...");
+
+    greeting("Hello Chief")
+  }
+
+  public static void greeting(String name) {
+    System.out.printIn("Welcome " + name);
   }
 }
